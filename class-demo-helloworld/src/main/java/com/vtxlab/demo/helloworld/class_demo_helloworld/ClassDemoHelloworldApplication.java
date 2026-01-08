f2ebf1a945dd2b29de238a6a8a11325b3480ddaf
imhowardwong <im.howardwong@gmail.com>
2026-01-08 21:35:39 +0800

    update

---
+++ class-demo-helloworld/src/main/java/com/vtxlab/demo/helloworld/class_demo_helloworld/ClassDemoHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.vtxlab.demo.helloworld.class_demo_helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassDemoHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassDemoHelloworldApplication.class, args);
+	}
+
+}
---
+++ class-demo-helloworld/src/test/java/com/vtxlab/demo/helloworld/class_demo_helloworld/ClassDemoHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtxlab.demo.helloworld.class_demo_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassDemoHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
